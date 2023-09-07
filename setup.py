ac0eb06a6873a316383885f9f1ff5bc26c337321
joao_almeida <jp080496@gmail.com>
2023-09-06 21:46:24 -0300

    sample-test

---
+++ setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/kennethreitz/setup.py
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    include_package_data=True,
+    name='sample',
+    version='0.1.0',
+    description='Sample package for Python-Guide.org',
+    long_description=readme,
+    author='Kenneth Reitz',
+    author_email='me@kennethreitz.com',
+    url='https://github.com/kennethreitz/samplemod',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=[
+        'pandas',
+        'pytest',
+    ],
+    python_requires='>=3.9',
+)
+
